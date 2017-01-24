db76a73278c892027e11a8e955d7b2659c03a8ce
HEBER-PC\heber <heber.navegante@gmail.com>
2017-01-24 14:15:13 -0200

    Singleton

---
+++ Learn.DesignPatterns/Learn.DesignPatterns.05_Singleton/Content/ChocolateBoiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.DesignPatterns._05_Singleton.Content
+{
+  public class ChocolateBoiler
+  {
+    private volatile static ChocolateBoiler uniqueInstance;
+
+    private bool empty;
+    private bool boiled;
+
+    private ChocolateBoiler()
+    {
+      empty = true;
+      boiled = false;
+    }
+
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    public static ChocolateBoiler GetInstance()
+    {
+      lock(uniqueInstance)
+      {
+        if (uniqueInstance == null) uniqueInstance = new ChocolateBoiler();
+
+        return uniqueInstance;
+      }
+    }
+
+    public void Fill()
+    {
+      if(IsEmpty())
+      {
+        empty = false;
+        boiled = false;
+      }
+    }
+
+    public void Drain()
+    {
+      if(!IsEmpty() && IsBoiled())
+      {
+        empty = true;
+      }
+    }
+
+    public void Boil()
+    {
+      if (!IsEmpty() && !IsBoiled()) boiled = true;
+    }
+
+    public bool IsEmpty()
+    {
+      return empty;
+    }
+
+    public bool IsBoiled()
+    {
+      return boiled;
+    }
+  }
+}
---
+++ Learn.DesignPatterns/Learn.DesignPatterns.05_Singleton/Content/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.DesignPatterns._05_Singleton.Content
+{
+  public class Singleton
+  {
+    private static Singleton uniqueInstace;
+
+    private Singleton()
+    {
+
+    }
+
+    public static Singleton UniqueInstance
+    {
+      get
+      {
+        if (uniqueInstace == null) uniqueInstace = new Singleton();
+
+        return uniqueInstace;
+      }
+    }
+  }
+}
